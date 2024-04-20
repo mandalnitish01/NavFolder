7e7e1506c706b6179056802118ac065bf70e19df
nitish <mandalnitish570@gmail.com>
2024-04-21 00:43:26 +0530

    1

--- navdesign/src/App.jsx
+++ navdesign/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './Components/Nav'
+import card from './CardComponents/card'
+
 
 
 function App() {
   return (
     <>
     <Nav LogoName="VNC" />
-    
+    <card/>
     </>
   )
 }
