d6370d87ecf1460f806f18d5d1a38bced4764bb8
nitish <mandalnitish570@gmail.com>
2024-04-21 00:10:25 +0530

    first commit

---
+++ navdesign/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './Components/Nav'
+
+
+function App() {
+  return (
+    <>
+    <Nav LogoName="VNC" />
+    
+    </>
+  )
+}
+export default App
